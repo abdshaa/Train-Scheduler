7fb855c0843cef1bd78619814accb0fb584334ff
abdul shaikh <raaqibshaikh@yahoo.com>
2019-09-11 22:05:43 -0500

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   // A variable to reference the database.
   var database = firebase.database();
 
-  // Variables for the onClick event
+  // Variables
   var name;
   var destination;
   var firstTrain;
@@ -46,9 +46,7 @@ $(document).ready(function() {
   });
 
   database.ref().on("child_added", function(childSnapshot) {
-    var nextArr;
     var minAway;
-    // Change year so first train comes before now
     var firstTrainNew = moment(
       childSnapshot.val().firstTrain,
       "hh:mm"
@@ -56,7 +54,6 @@ $(document).ready(function() {
     // Difference between the current and firstTrain
     var diffTime = moment().diff(moment(firstTrainNew), "minutes");
     var remainder = diffTime % childSnapshot.val().frequency;
-    // Minutes until next train
     var minAway = childSnapshot.val().frequency - remainder;
     // Next train time
     var nextTrain = moment().add(minAway, "minutes");
@@ -78,7 +75,6 @@ $(document).ready(function() {
   });
 
   database.ref().on("child_added", function(snapshot) {
-    // Change the HTML to reflect
     $("#name-display").html(snapshot.val().name);
     $("#email-display").html(snapshot.val().email);
     $("#age-display").html(snapshot.val().age);
